45fccf4fc3fd64f1789315b712cb6746973f6e93
Jaiden Gerig <jaideng123@yahoo.com>
2019-09-29 21:33:42 -0700

    1. Basic Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,67 @@
-
+// Created based on https://learnopengl.com/Getting-started
 #include <GLFW/glfw3.h>
 #include <iostream>
 using namespace std;
-
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
+void processInput(GLFWwindow *window);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+// OpenGL acts as a state machine
 int main()
 {
-    cout << "A nondescript C++ program using the GLFW library, coded and built using VSCode" << endl;
-    cout << "Initializing Graphics Library FramWork (GLFW)" << endl;
+    glfwInit();
+    // Set to OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    if (!glfwInit())
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
     {
-        return -1;
-    }
-
-    auto gameWindow = glfwCreateWindow(640, 480, "GLFW Test App", NULL, NULL);
-    if (!gameWindow)
-    {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    glfwMakeContextCurrent(gameWindow);
-    glfwSetKeyCallback(gameWindow, key_callback);
-    float currentRotationX = 0.f;
-    float previousTime = 0.f;
-    // Main loop
-    while (!glfwWindowShouldClose(gameWindow))
+    glfwMakeContextCurrent(window);
+    // Set size of the rendering window(viewport)
+    // (X,Y,Len,Width) from top left corner
+    glViewport(0, 0, 800, 600);
+
+    // Callback to re-adjust viewport on window resize
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Sets color to clear screen with
+    //(R,G,B,A)
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    // Render Loop
+    while (!glfwWindowShouldClose(window))
     {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(gameWindow, &width, &height);
-        ratio = width / (float)height;
-        glViewport(0, 0, width, height);
+        processInput(window);
+
+        // Clears Color buffer
+        // The possible bits for glClear() are:
+        // GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT and GL_STENCIL_BUFFER_BIT
         glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
 
-        float time = (float)glfwGetTime();
-        float deltaTime = time - previousTime;
-        previousTime = time;
-
-        if (glfwGetKey(gameWindow, GLFW_KEY_LEFT))
-        {
-            currentRotationX += deltaTime * -50.f;
-        }
-        else if (glfwGetKey(gameWindow, GLFW_KEY_RIGHT))
-        {
-            currentRotationX += deltaTime * 50.f;
-        }
-
-        glRotatef(currentRotationX, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.7f, 0.f);
-        glEnd();
-        glfwSwapBuffers(gameWindow);
+        // Checks for keyboard, mouse, etc.
         glfwPollEvents();
+        // Swap pixel color buffers for window
+        glfwSwapBuffers(window);
     }
 
+    // Clean up GLFW resources
     glfwTerminate();
+
     return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
