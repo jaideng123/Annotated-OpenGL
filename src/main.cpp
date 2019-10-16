c7092c0bc4d559b8151da63bb4fdb6f4dfc7de04
Jaiden Gerig <jaideng123@yahoo.com>
2019-10-16 14:01:38 -0500

    2c. Multiple triangles

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <streambuf>
+#include <vector>
 using namespace std;
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
@@ -12,6 +13,8 @@ string readFileContents(string filename);
 void checkSuccessfulShaderCompilation(int shaderId);
 int generateAndCompileShader(string sourceFileLocation, int shaderType);
 void checkSuccessfulShaderLink(int shaderId);
+int generateVAO(vector<float> vertices, vector<unsigned int> indices);
+
 // OpenGL acts as a state machine
 int main()
 {
@@ -61,48 +64,22 @@ int main()
 
     // Defined in Normalized Device Coordinates (between -1 and 1)
     // Eventually tranformed into screenspace via glViewport
-    float vertices[] = {
-        0.5f, 0.5f, 0.0f,   // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f   // top left
-    };
-    unsigned int indices[] = {
-        // note that we start from 0!
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
+    vector<float> vertices = {
+        0.0f, 0.5f, 0.0f, // top
+        0.5f, 0.0f, 0.0f, // right
+        -0.5f, 0.0f, 0.0f // left
     };
 
-    // Similar to VBO stores:
-    // - Calls to glEnableVertexAttribArray or glDisableVertexAttribArray.
-    // - Vertex attribute configurations via glVertexAttribPointer
-    // - ertex buffer objects associated with vertex attributes by calls to glVertexAttribPointer
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
+    vector<float> vertices2 = {
+        0.0f, 0.0f, 0.0f,  // top
+        0.5f, -0.5f, 0.0f, // right
+        -0.5f, -0.5f, 0.0f // left
+    };
 
-    // 0. copy our vertices array in a buffer for OpenGL to use
-    // Generate 1 Buffer binding point and assign ID to VBO
-    // VBO stores large # of vertices in GPU memory
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    // Bind ID to a new buffer object with GL_BUFFER_ARRAY buffer type
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // Copy vertex data into buffer
-    // GL_STATIC_DRAW: the data will most likely not change at all or very rarely.
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    vector<unsigned int> indices = {0, 1, 2};
 
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // 1. then set the vertex attributes pointers
-    // Tells opengl how to interpret vertex data
-    // (location(in shader), size of vertex, datatype, normalized, stride(length of vertex data), offset)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    // Enable array in shader at location 0
-    glEnableVertexAttribArray(0);
+    int VAO1 = generateVAO(vertices, indices);
+    int VAO2 = generateVAO(vertices2, indices);
 
     // Set size of the rendering window(viewport)
     // (X,Y,Len,Width) from top left corner
@@ -130,7 +107,9 @@ int main()
 
         // use our shader program when we want to render an object
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+        glBindVertexArray(VAO1);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(VAO2);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0); // Unbind vertex array
 
@@ -144,6 +123,42 @@ int main()
     glfwTerminate();
 
     return 0;
+}
+
+int generateVAO(vector<float> vertices, vector<unsigned int> indices)
+{
+    // Similar to VBO stores:
+    // - Calls to glEnableVertexAttribArray or glDisableVertexAttribArray.
+    // - Vertex attribute configurations via glVertexAttribPointer
+    // - vertex buffer objects associated with vertex attributes by calls to glVertexAttribPointer
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // 0. copy our vertices array in a buffer for OpenGL to use
+    // Generate 1 Buffer binding point and assign ID to VBO
+    // VBO stores large # of vertices in GPU memory
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    // Bind ID to a new buffer object with GL_BUFFER_ARRAY buffer type
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Copy vertex data into buffer
+    // GL_STATIC_DRAW: the data will most likely not change at all or very rarely.
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+    // 1. then set the vertex attributes pointers
+    // Tells opengl how to interpret vertex data
+    // (location(in shader), size of vertex, datatype, normalized, stride(length of vertex data), offset)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    // Enable array in shader at location 0
+    glEnableVertexAttribArray(0);
+
+    return VAO;
 }
 
 string readFileContents(string filename)
